c2f783fd3a6c8456c68c232dad8b128f37d25e56
MiaowFISH <miaowfish@qq.com>
2026-08-23 02:04:43 +0800

    chore: add lint rules

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: ["**/node_modules/**", "**/.git/**", "legacy/**", "vendor/**/tests/**", "providers/**"],
+    exclude: ["**/node_modules/**", "**/.git/**", "legacy/**", "tools/**"],
   },
 });
